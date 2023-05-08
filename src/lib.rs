e064af1c8426c1a7ecf62d5ddfc598473a98b970
Bradley Lyman <lyman.brad3211@gmail.com>
2023-05-07 17:45:39 -0700

    add the create_system_allocator method

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,14 @@ mod memory_allocator;
 mod memory_properties;
 mod pretty_wrappers;
 
+use {
+    self::{
+        device_memory::DeviceMemory,
+        pretty_wrappers::{PrettyBitflag, PrettySize},
+    },
+    ash::vk,
+};
+
 pub use self::{
     allocation::Allocation,
     allocation_requirements::{
@@ -22,7 +30,79 @@ pub use self::{
     },
     memory_properties::MemoryProperties,
 };
-use self::{
-    device_memory::DeviceMemory,
-    pretty_wrappers::{PrettyBitflag, PrettySize},
-};
+
+/// Create an opinionated system allocator for GPU memoy.
+///
+/// # Safety
+///
+/// Unsafe because:
+/// - The application must keep the device alive for as long as the allocator is
+///   alive.
+/// - The application must free any memory it allocates prior to dropping the
+///   memory allocator or device.
+pub unsafe fn create_system_allocator(
+    instance: &ash::Instance,
+    device: ash::Device,
+    physical_device: vk::PhysicalDevice,
+) -> MemoryAllocator {
+    let memory_properties = MemoryProperties::new(instance, physical_device);
+
+    let device_allocator = into_shared(TraceAllocator::new(
+        instance,
+        physical_device,
+        DeviceAllocator::new(device.clone()),
+        "Device Allocator",
+    ));
+
+    let small_page_size = 1024; // 1kb
+    let small_chunk_size = small_page_size * 64; // 64kb
+    let medium_page_size = small_chunk_size; // 64kb
+    let medium_chunk_size = medium_page_size * 128; // 8mb
+    let root_page_size = medium_chunk_size; // 8mb
+    let root_chunk_size = medium_chunk_size * 256; // 2gb
+
+    let large_chunk_pool_allocator = into_shared(SizedAllocator::new(
+        root_chunk_size,
+        PoolAllocator::new(
+            memory_properties.clone(),
+            root_chunk_size,
+            root_page_size,
+            device_allocator.clone(),
+        ),
+        device_allocator.clone(),
+    ));
+
+    let medium_chunk_pool_allocator = into_shared(SizedAllocator::new(
+        medium_chunk_size,
+        PoolAllocator::new(
+            memory_properties.clone(),
+            medium_chunk_size,
+            medium_page_size,
+            large_chunk_pool_allocator.clone(),
+        ),
+        large_chunk_pool_allocator,
+    ));
+
+    let small_chunk_pool_allocator = SizedAllocator::new(
+        small_chunk_size,
+        PoolAllocator::new(
+            memory_properties,
+            small_chunk_size,
+            small_page_size,
+            medium_chunk_pool_allocator.clone(),
+        ),
+        medium_chunk_pool_allocator,
+    );
+
+    let dedicated_allocator =
+        DedicatedAllocator::new(small_chunk_pool_allocator, device_allocator);
+
+    let system_allocator = TraceAllocator::new(
+        instance,
+        physical_device,
+        dedicated_allocator,
+        "Application Allocator",
+    );
+
+    MemoryAllocator::new(instance, device, physical_device, system_allocator)
+}
--- tests/test_memory_mapped_buffer.rs
+++ tests/test_memory_mapped_buffer.rs
@@ -2,74 +2,11 @@
 //! be written and read from the buffer.
 
 use {
-    anyhow::Result,
-    ash::vk,
-    ccthw_ash_allocator::{
-        into_shared, DedicatedAllocator, DeviceAllocator, MemoryAllocator,
-        MemoryProperties, PoolAllocator, SizedAllocator, TraceAllocator,
-    },
-    ccthw_ash_instance::VulkanHandle,
-    scopeguard::defer,
+    anyhow::Result, ash::vk, ccthw_ash_allocator::create_system_allocator,
+    ccthw_ash_instance::VulkanHandle, scopeguard::defer,
 };
 
 mod common;
-
-unsafe fn create_allocator(
-    instance: &ash::Instance,
-    device: ash::Device,
-    physical_device: vk::PhysicalDevice,
-) -> MemoryAllocator {
-    let memory_properties = MemoryProperties::new(instance, physical_device);
-
-    let device_allocator = into_shared(TraceAllocator::new(
-        instance,
-        physical_device,
-        DeviceAllocator::new(device.clone()),
-        "Device Allocator",
-    ));
-
-    let root_chunk_size = 1024 * 1024 * 1024; // kb -> mb -> gb
-    let root_page_size = root_chunk_size / 128;
-
-    let large_chunk_pool_allocator = into_shared(PoolAllocator::new(
-        memory_properties.clone(),
-        root_chunk_size,
-        root_page_size,
-        device_allocator.clone(),
-    ));
-
-    let small_chunk_size = root_page_size;
-    let small_page_size = small_chunk_size / 256;
-
-    let small_chunk_pool_allocator = PoolAllocator::new(
-        memory_properties,
-        small_chunk_size,
-        small_page_size,
-        large_chunk_pool_allocator.clone(),
-    );
-
-    let sized_allocator = SizedAllocator::new(
-        root_chunk_size,
-        SizedAllocator::new(
-            small_chunk_size,
-            small_chunk_pool_allocator,
-            large_chunk_pool_allocator,
-        ),
-        device_allocator.clone(),
-    );
-
-    let dedicated_allocator =
-        DedicatedAllocator::new(sized_allocator, device_allocator);
-
-    let system_allocator = TraceAllocator::new(
-        instance,
-        physical_device,
-        dedicated_allocator,
-        "Application Allocator",
-    );
-
-    MemoryAllocator::new(instance, device, physical_device, system_allocator)
-}
 
 #[repr(C, packed)]
 struct ExampleData {
@@ -82,7 +19,7 @@ pub fn test_mapped_buffer() -> Result<()> {
     log::info!("{}", device);
 
     let mut allocator = unsafe {
-        create_allocator(
+        create_system_allocator(
             device.instance.ash(),
             device.logical_device.raw().clone(),
             *device.logical_device.physical_device().raw(),
@@ -159,7 +96,7 @@ pub fn test_repeated_mapping() -> Result<()> {
     log::info!("{}", device);
 
     let mut allocator = unsafe {
-        create_allocator(
+        create_system_allocator(
             device.instance.ash(),
             device.logical_device.raw().clone(),
             *device.logical_device.physical_device().raw(),
